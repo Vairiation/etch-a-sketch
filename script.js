78172887fa86888546eaa8cd1f1d05e9ae22f12e
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-09-04 16:03:57 -0500

    Create createGrid() function

---
+++ script.js
@@ -0,0 +1,8 @@
+function createGrid() {
+    const square = document.createElement('div)');
+    
+    square.className = 'grid';
+    square.style.height = `${1/16}vh`;
+    square.style.width = `${1/16}vw`;
+    square
+}
