27f33c80b64b27a15844ed2cc0f651beb6280936
Weston Shelander <130246805+Vairiation@users.noreply.github.com>
2026-09-07 14:05:37 -0500

    Create getRandomColor() to set random fill color on hover

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ function createGrid(gridNum) {
             
             square.addEventListener('mouseenter', () => {
                 square.style.transitionDuration = '0s';
-                square.style.backgroundColor = 'black';
+                square.style.backgroundColor = `${getRandomColor()}`;
             })
             square.addEventListener('mouseleave', () => {
                 square.style.transitionDuration = '5s';
@@ -72,6 +72,16 @@ function createGrid(gridNum) {
     
     body.appendChild(setGridBtn);
     body.appendChild(grid);
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)]
+    }
+    return color;
 }
 
 createGrid(16);
